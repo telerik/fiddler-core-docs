2a460ecf98b105f494d924a47c7e46aa76b8d363
Nikolay Iliev <nikolay.iliev@telerik.com>
2021-03-05 10:46:47 +0200

    cached gitignore

--- _plugins/link_issues.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module GithubIssuesLink
-    def link_github_issues(input, repo)
-        input.gsub(/(?<!&)(#(\d+))/, "<a href='https://github.com/#{repo}/issues/\\2'>\\1</a>")
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::GithubIssuesLink)
--- _plugins/liquid_parser.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module FrontMatterLiquidParser
-    def parse_front_matter(input)
-      Liquid::Template.parse(input).render(@context)
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::FrontMatterLiquidParser)
